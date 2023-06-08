e5f9c7abdb62e1d05e049b7e736c822444c4517a
Celine Sayegh <celinealsayegh@gmail.com>
2023-06-08 18:22:35 +0300

    project completed

---
+++ script.js
@@ -0,0 +1,25 @@
+const output=document.querySelector(".output");
+const result=document.querySelector(".result");
+const buttons=document.querySelectorAll("button");
+buttons.forEach(button=>{
+    button.addEventListener("click", calculate)
+})
+function calculate(){
+  var buttonText=this.innerText;
+  if(buttonText==="AC"){
+    output.innerText="";
+    result.innerText="0";
+    return
+  }
+  if(buttonText==="DEL"){
+    output.innerText=output.innerText.substring(0,output.innerText.length-1);
+    return;
+  }
+  if(buttonText==="="){
+    result.innerHTML=eval(output.innerText);
+  }
+  else{
+    output.innerHTML+=buttonText;
+  }
+
+}
